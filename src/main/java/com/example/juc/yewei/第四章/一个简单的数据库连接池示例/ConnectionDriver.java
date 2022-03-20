709ab9415cfc61d1ed0a20d2d63954717305fb59
airoger <czzx201101136>
2022-03-20 18:51:22 +0800

    innit

---
+++ src/main/java/com/example/juc/yewei/第四章/一个简单的数据库连接池示例/ConnectionDriver.java
@@ -0,0 +1,27 @@
+package com.example.juc.yewei.第四章.一个简单的数据库连接池示例;
+
+import sun.reflect.Reflection;
+
+import java.lang.reflect.*;
+import java.sql.Connection;
+import java.util.concurrent.TimeUnit;
+
+public class ConnectionDriver {
+    static class ConnectionHandler implements InvocationHandler{
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (method.getName().equals("commit")){
+                TimeUnit.MILLISECONDS.sleep(100);
+            }
+            return null;
+        }
+    }
+
+    //connection是接口，交由各数据库的驱动去实现的，这里就不做实现，简单用动态代理返回一个代理对象
+    public static final Connection createConnection() {
+        return (Connection) Proxy.newProxyInstance(ConnectionDriver.class.getClassLoader(),
+                new Class[]{Connection.class},
+                new ConnectionHandler());
+    }
+
+}
