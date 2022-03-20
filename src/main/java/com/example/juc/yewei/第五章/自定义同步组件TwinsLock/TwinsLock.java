ce658ef2d99db23a0b9d719fceb91bddf1035daa
airoger <czzx201101136>
2022-03-20 18:49:49 +0800

    innit

---
+++ src/main/java/com/example/juc/yewei/第五章/自定义同步组件TwinsLock/TwinsLock.java
@@ -0,0 +1,71 @@
+package com.example.juc.yewei.第五章.自定义同步组件TwinsLock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class TwinsLock implements Lock {
+    private static final Sync sync = new Sync(2);
+
+    static class Sync extends AbstractQueuedSynchronizer{
+        //构造方法
+        Sync (int count){
+            if (count <0){
+                throw new IllegalArgumentException("count must large than zero");
+            }
+            setState(count);
+        }
+
+        //重写tryAcquireShared方法
+        public int tryAcquireShared(int reduceCount){
+            for(;;){
+                int current = getState();
+                int newCount = current - reduceCount;
+                if (newCount < 0 || compareAndSetState(current,newCount)) {
+                    return newCount;
+                }
+            }
+        }
+
+        public boolean tryReleaseShared(int returnCount) {
+            for (;;) {
+                int current = getState();
+                int newCount = current + returnCount;
+                if (compareAndSetState(current, newCount))
+                { return true; }
+            }
+        }
+
+    }
+
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+}
